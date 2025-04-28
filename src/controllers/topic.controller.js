b0b9c9037e4beb57ae922e8b54a9e25527d60751
Richard Lambert <hello@richardlambert.dev>
2025-04-28 15:02:52 +0100

    :recycle: refactor topics - breakout controller and model

--- src/controllers/topic.controller.js
+++ src/controllers/topic.controller.js
@@ -1,7 +1,7 @@
-import db from '../database/connection.js';
 import { withTryCatch } from '../utilities/index.js';
+import { selectAllTopics } from '../models/topic.model.js';
 
 export const getTopics = withTryCatch(async (request, response, next) => {
-  const { rows: topics } = await db.query('SELECT slug, description FROM topics;');
+  const topics = await selectAllTopics();
   return response.status(200).json({ topics });
 });
