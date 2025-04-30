1028578bb7bfcaecd91de1d3b1c35ed6438d3568
Richard Lambert <hello@richardlambert.dev>
2025-04-30 08:32:43 +0100

    :sparkles: 17 patch-comment-by-id

--- src/schemas/comment.schema.js
+++ src/schemas/comment.schema.js
@@ -5,3 +5,10 @@ export const deleteCommentByIdSchema = Joi.object({
     .pattern(new RegExp(/^\d{1,}$/))
     .required(),
 }).required();
+
+export const patchCommentByIdSchema = Joi.object({
+  id: Joi.string()
+    .pattern(new RegExp(/^\d{1,}$/))
+    .required(),
+  inc_votes: Joi.number().integer().required(),
+}).required();
