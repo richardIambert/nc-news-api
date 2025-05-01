54d09109339dac8513a8b873d5e73f3ffe77b2b8
Richard Lambert <hello@richardlambert.dev>
2025-05-01 10:35:02 +0100

    :mute: remove console.log

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,9 +5,6 @@ const ENV = process.env.NODE_ENV || 'development';
 
 config({ path: `${import.meta.dirname}/../../.env.${ENV}` });
 
-console.log(ENV, '<<<< env');
-console.log(process.env.PGDATABASE, '<<<< pgdatabase');
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
